299aff858c06dc7ce96fe195837389a802866c81
Changyu <liuchangyu1111@gmail.com>
2020-11-11 16:46:46 +0800

    update --all

--- trainer.py
+++ trainer.py
@@ -158,6 +158,10 @@ class Trainer:
         discriminator.train()
         generator.train()
 
+        # Loading PSNR pre training model
+        if args.resume_PSNR:
+            self.resume_resnet()
+
         # Pre-train generator using raw l1 loss
         logger.print_info("[*] Start training PSNR model based on L1 loss.")
         # Save the generator model based on MSE pre training to speed up the training time
@@ -220,3 +224,111 @@ class Trainer:
             torch.save(generator.state_dict(), f"./weight/ResNet_{args.upscale_factor}x.pth")
             print(f"[*] Training PSNR model done! Saving PSNR model weight to "
                   f"`./weight/ResNet_{args.upscale_factor}x.pth`.")
+
+            # After training the PSNR model, set the initial iteration to 0.
+            args.start_epoch = 0
+
+            # Loading GAN checkpoint
+            if args.resume:
+                self.resume_gan()
+
+            # Train GAN model.
+            print(f"[*] Staring training GAN model!")
+            print(f"[*] Training for {epochs} epochs.")
+            # Writer train GAN model log.
+            if args.start_epoch == 0:
+                with open(f"GAN_{args.upscale_factor}x_Loss.csv", "w+") as f:
+                    writer = csv.writer(f)
+                    writer.writerow(["Epoch", "D Loss", "G Loss"])
+
+            for epoch in range(args.start_epoch, epochs):
+                progress_bar = tqdm(enumerate(dataloader), total=len(dataloader))
+                g_avg_loss = 0.
+                d_avg_loss = 0.
+                for i, (input, target) in progress_bar:
+                    lr = input.to(device)
+                    hr = target.to(device)
+                    batch_size = lr.size(0)
+                    real_label = torch.full((batch_size, 1), 1, dtype=lr.dtype, device=device)
+                    fake_label = torch.full((batch_size, 1), 0, dtype=lr.dtype, device=device)
+
+                    ##############################################
+                    # (1) Update D network: maximize - E(lr)[1- log(D(hr, sr))] - E(sr)[log(D(sr, hr))]
+                    ##############################################
+                    # Set discriminator gradients to zero.
+                    discriminator.zero_grad()
+
+                    # Generate a super-resolution image
+                    sr = generator(lr)
+
+                    # Train with real high resolution image.
+                    hr_output = discriminator(hr)  # Train real image.
+                    sr_output = discriminator(sr.detach())  # No train fake image.
+                    # Adversarial loss for real and fake images (relativistic average GAN)
+                    errD_hr = adversarial_criterion(hr_output - torch.mean(sr_output), real_label)
+                    errD_sr = adversarial_criterion(sr_output - torch.mean(hr_output), fake_label)
+                    errD = (errD_sr + errD_hr) / 2
+                    errD.backward()
+                    D_x = hr_output.mean().item()
+                    D_G_z1 = sr_output.mean().item()
+                    optimizerD.step()
+
+                    ##############################################
+                    # (2) Update G network: maximize - E(lr)[log(D(hr, sr))] - E(sr)[1- log(D(sr, hr))]
+                    ##############################################
+                    # Set generator gradients to zero
+                    generator.zero_grad()
+
+                    # According to the feature map, the root mean square error is regarded as the content loss.
+                    vgg_loss = vgg_criterion(sr, hr)
+                    # Train with fake high resolution image.
+                    hr_output = discriminator(hr.detach())  # No train real fake image.
+                    sr_output = discriminator(sr)  # Train fake image.
+                    # Adversarial loss (relativistic average GAN)
+                    adversarial_loss = adversarial_criterion(sr_output - torch.mean(hr_output), real_label)
+                    # Pixel level loss between two images.
+                    l1_loss = pix_criterion(sr, hr)
+                    errG = 10 * l1_loss + vgg_loss + 5e-3 * adversarial_loss
+                    errG.backward()
+                    D_G_z2 = sr_output.mean().item()
+                    optimizerG.step()
+
+                    # Dynamic adjustment of learning rate
+                    schedulerD.step()
+                    schedulerG.step()
+
+                    d_avg_loss += errD.item()
+                    g_avg_loss += errG.item()
+
+                    progress_bar.set_description(f"[{epoch + 1}/{epochs}][{i + 1}/{len(dataloader)}] "
+                                                 f"Loss_D: {errD.item():.6f} Loss_G: {errG.item():.6f} "
+                                                 f"D(HR): {D_x:.6f} D(G(LR)): {D_G_z1:.6f}/{D_G_z2:.6f}")
+
+                    # record iter.
+                    total_iter = len(dataloader) * epoch + i
+
+                    # The image is saved every 5000 iterations.
+                    if (total_iter + 1) % args.save_freq == 0:
+                        vutils.save_image(hr, os.path.join("./output/hr", f"ResNet_{total_iter + 1}.bmp"))
+                        vutils.save_image(sr, os.path.join("./output/sr", f"ResNet_{total_iter + 1}.bmp"))
+
+                # The model is saved every 1 epoch.
+                torch.save({"epoch": epoch + 1,
+                            "optimizer": optimizerD.state_dict(),
+                            "state_dict": discriminator.state_dict()
+                            }, f"./weight/netD_{args.upscale_factor}x_checkpoint.pth")
+                torch.save({"epoch": epoch + 1,
+                            "optimizer": optimizerG.state_dict(),
+                            "state_dict": generator.state_dict()
+                            }, f"./weight/netG_{args.upscale_factor}x_checkpoint.pth")
+
+                # Writer training log
+                with open(f"GAN_{args.upscale_factor}x_Loss.csv", "a+") as f:
+                    writer = csv.writer(f)
+                    writer.writerow([epoch + 1,
+                                     d_avg_loss / len(dataloader),
+                                     g_avg_loss / len(dataloader)])
+
+            torch.save(generator.state_dict(), f"./weight/GAN_{args.upscale_factor}x.pth")
+            logger.print_info(f"[*] Training GAN model done! Saving GAN model weight "
+                              f"to `./weight/GAN_{args.upscale_factor}x.pth`.")
