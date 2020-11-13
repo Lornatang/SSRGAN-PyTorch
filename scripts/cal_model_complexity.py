70239da2082b044eddf7a9e98f0c639f2d7c85f0
Changyu <liuchangyu1111@gmail.com>
2020-11-13 20:33:04 +0800

    Complexity calculation

--- scripts/cal_model_complexity.py
+++ scripts/cal_model_complexity.py
@@ -21,26 +21,26 @@ from ssrgan.utils import select_device
 model_names = sorted(name for name in models.__dict__
                      if name.islower() and not name.startswith("__")
                      and callable(models.__dict__[name]))
-print(model_names)
 
-parser = argparse.ArgumentParser(description="Research and application of GAN based super resolution "
-                                             "technology for pathological microscopic images.")
-# model parameters
-parser.add_argument("-a", "--arch", metavar="ARCH", default="bionet",
-                    choices=model_names,
-                    help="model architecture: " +
-                         " | ".join(model_names) +
-                         " (default: bionet)")
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Research and application of GAN based super resolution "
+                                                 "technology for pathological microscopic images.")
+    # model parameters
+    parser.add_argument("-a", "--arch", metavar="ARCH", default="bionet",
+                        choices=model_names,
+                        help="model architecture: " +
+                             " | ".join(model_names) +
+                             " (default: bionet)")
+    args = parser.parse_args()
 
-device = select_device("cpu")
-model = models.__dict__[args.arch]().to(device)
+    device = select_device("cpu")
+    model = models.__dict__[args.arch]().to(device)
 
-size = (3, 54, 54)
-flops, params = get_model_complexity_info(model, size, as_strings=True, print_per_layer_stat=True)
-print(f"                   Summary                     ")
-print(f"-----------------------------------------------")
-print(f"|       Model       |    Params   |   FLOPs   |")
-print(f"-----------------------------------------------")
-print(f"|{model.__class__.__name__.center(19):19}|{params.center(13):13}|{flops.center(11):11}|")
-print(f"-----------------------------------------------")
+    size = (3, 54, 54)
+    flops, params = get_model_complexity_info(model, size, as_strings=True, print_per_layer_stat=True)
+    print(f"                   Summary                     ")
+    print(f"-----------------------------------------------")
+    print(f"|       Model       |    Params   |   FLOPs   |")
+    print(f"-----------------------------------------------")
+    print(f"|{model.__class__.__name__.center(19):19}|{params.center(13):13}|{flops.center(11):11}|")
+    print(f"-----------------------------------------------")
