578329246a64a189f555d7c6fce1afb15ed3491e
Liu <liuchangyu1111@gmail.com>
2021-03-18 17:07:13 +0800

    Fix `TypeError: load_state_dict() got multiple values for argument 'state_dict'`

--- ssrgan/utils/common.py
+++ ssrgan/utils/common.py
@@ -51,6 +51,6 @@ def configure(args):
         model = models.__dict__[args.arch]()
     if args.model_path:
         logger.info(f"You loaded the specified weight. Load weights from `{args.model_path}`")
-        model.load_state_dict(torch.load(args.model_path, map_location=torch.device("cpu")), state_dict=False)
+        model.load_state_dict(torch.load(args.model_path, map_location=torch.device("cpu")), strict=False)
 
     return model
