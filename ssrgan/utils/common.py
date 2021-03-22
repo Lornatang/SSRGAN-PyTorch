5910763219ecf97f3b3f3563508c6c40628548fb
Liu <liuchangyu1111@gmail.com>
2021-03-22 09:56:32 +0800

    Add progress

--- ssrgan/utils/common.py
+++ ssrgan/utils/common.py
@@ -19,7 +19,7 @@ import torch
 import ssrgan.models as models
 
 __all__ = [
-    "create_folder", "configure"
+    "create_folder", "configure", "save_checkpoint", "AverageMeter", "ProgressMeter"
 ]
 
 logger = logging.getLogger(__name__)
@@ -54,3 +54,54 @@ def configure(args):
         model.load_state_dict(torch.load(args.model_path, map_location=torch.device("cpu")), strict=False)
 
     return model
+
+
+# Copy from https://github.com/pytorch/examples/blob/master/imagenet/main.py
+def save_checkpoint(state, is_best: bool, source_filename: str, target_filename: str):
+    torch.save(state, source_filename)
+    if is_best:
+        torch.save(state["state_dict"], target_filename)
+
+
+# Copy from https://github.com/pytorch/examples/blob/master/imagenet/main.py
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+# Copy from https://github.com/pytorch/examples/blob/master/imagenet/main.py
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
