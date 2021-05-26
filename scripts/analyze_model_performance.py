4c7dbea492cda698d10b28f0fb001c64c408dc74
Liu <liuchangyu1111@gmail.com>
2021-05-26 16:19:58 +0800

    It is mainly used to analyze the performance of the model

--- scripts/analyze_model_performance.py
+++ scripts/analyze_model_performance.py
@@ -12,49 +12,84 @@
 # limitations under the License.
 # ==============================================================================
 import argparse
+import logging
+import random
 import time
 
 import torch
+import torch.backends.cudnn as cudnn
 
 import ssrgan.models as models
 
+# Find all available models.
 model_names = sorted(name for name in models.__dict__ if name.islower() and not name.startswith("__") and callable(models.__dict__[name]))
 
-parser = argparse.ArgumentParser("Research on the technology of digital pathological image super-resolution.")
-parser.add_argument("-a", "--arch", metavar="ARCH", default="pmigan",
-                    choices=model_names,
-                    help="Model architecture: " +
-                         " | ".join(model_names) +
-                         ". (Default: `pmigan`)")
-parser.add_argument("-i", "--image-size", type=int, default=64,
-                    help="Image size of low-resolution. (Default: 64)")
-parser.add_argument("--mode", default="cpu", type=bool,
-                    help="Use GPU analyze model performance.")
+# It is a convenient method for simple scripts to configure the log package at one time.
+logger = logging.getLogger(__name__)
+logging.basicConfig(format="[ %(levelname)s ] %(message)s", level=logging.DEBUG)
 
 
-def main():
-    args = parser.parse_args()
+def main(args):
+    # In order to make the model repeatable, the first step is to set random seeds, and the second step is to set convolution algorithm.
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
 
+    # Build a super-resolution model, if model_ If path is defined, the specified model weight will be loaded.
     model = models.__dict__[args.arch]()
+    # Switch model to eval mode.
     model.eval()
-    data = torch.ones([1, 3, args.image_size, args.image_size])
 
-    if args.mode == "gpu":
-        model = model.cuda("0")
-        data = data.cuda("0")
+    # If the GPU is available, load the model into the GPU memory. This speed.
+    if not torch.cuda.is_available():
+        logger.warning("Using CPU, this will be slow.")
+    if args.gpu is not None:
+        torch.cuda.set_device(args.gpu)
+        model = model.cuda(args.gpu)
+        # Setting this flag allows the built-in auto tuner of cudnn to automatically find the most efficient algorithm suitable
+        # for the current configuration, so as to optimize the operation efficiency.
+        cudnn.benchmark = True
+        # Ensure that every time the same input returns the same result.
+        cudnn.deterministic = True
 
-    for _ in range(5):
+    # Create an image that conforms to the normal distribution.
+    data = torch.randn([1, 3, args.image_size, args.image_size], requires_grad=False)
+
+    # If there is a GPU, the data will be loaded into the GPU memory.
+    if args.gpu is not None:
+        data = data.cuda(args.gpu, non_blocking=True)
+
+    # It only needs to reconstruct the low resolution image without the gradient information of the reconstructed image.
+    with torch.no_grad():
         start = time.time()
         _ = model(data)
+        # Waits for all kernels in all streams on a CUDA device to complete.
         torch.cuda.synchronize()
-        end = time.time()
-        print(f"Time:{(end - start) * 1000:.2f}ms")
+        print(f"Time:{(time.time() - start) * 1000:.2f}ms.")
 
-    with torch.autograd.profiler.profile(enabled=True, use_cuda=True, record_shapes=False, profile_memory=False) as prof:
+    # Context manager that manages autograd profiler state and holds a summary of results.
+    with torch.autograd.profiler.profile(enabled=True, use_cuda=args.gpu, record_shapes=False, profile_memory=False) as profile:
         _ = model(data)
-    print(prof.table())
-    prof.export_chrome_trace("profile.json")
+    print(profile.table())
+    # Open Chrome browser and enter in the address bar `chrome://tracing`
+    profile.export_chrome_trace("profile.json")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--arch", metavar="ARCH", default="pmigan",
+                        choices=model_names,
+                        help="Model architecture: " +
+                             " | ".join(model_names) +
+                             ". (Default: `pmigan`)")
+    parser.add_argument("-i", "--image-size", type=int, default=54, help="Image size of low-resolution. (Default: 54)")
+    parser.add_argument("--seed", default=666, type=int,
+                        help="Seed for initializing training. (Default: 666)")
+    parser.add_argument("--gpu", default=None, type=int,
+                        help="GPU id to use.")
+    args = parser.parse_args()
+
+    logger.info("ScriptEngine:")
+    logger.info("\tAPI version .......... 0.1.4")
+    logger.info("\tBuild ................ 2021.05.26\n")
+
+    main(args)
